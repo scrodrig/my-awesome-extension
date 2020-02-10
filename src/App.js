db25e5399022656d39351939b5fef10fba34383d
Schubert Rodriguez <schubert_rodriguez@crgl-thirdparty.com>
2020-02-10 12:54:11 -0500

    configure extension to be a new tab ext

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*global chrome*/
+import React, { Button } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+
+                {/* <Button
+                    onClick={() => {
+                        chrome.tabs.getCurrent(tab => {
+                            chrome.tabs.update(tab.id, {
+                                url: 'chrome-search://local-ntp/local-ntp.html'
+                            })
+                        })
+                    }}
+                >
+                    Chrome Tab
+                </Button> */}
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
